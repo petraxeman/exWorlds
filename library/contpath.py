f2f990f53b04acae827f8632aa12fd8621cd4786
petraxeman <pawel.sultanoff@yandex.ru>
2025-02-16 23:45:06 +0300

    Safety load ContentPath added. Rewrited table/get_tables.py

--- library/contpath.py
+++ library/contpath.py
@@ -9,6 +9,8 @@ class ParsePathException(Exception):
 class VerifyPathException(Exception):
     pass
 
+class IntegrityPathException(Exception):
+    pass
 
 
 class ContentPath:
@@ -20,10 +22,12 @@ class ContentPath:
         else:
             self.fpack = self.category + self.pack
         
-        self.pack_exists = True if self.pack else False
-        self.addon_exists = True if self.addon else False
-        self.table_exists = True if self.table else False
-        self.note_exists = True if self.note else False
+        self.availables = {
+            "pack": True if self.pack else False,
+            "addon": True if self.addon else False,
+            "table": True if self.table else False,
+            "note": True if self.note else False,
+        }
     
     def to_str(self):
         points = [self.fpack, self.table, self.note]
@@ -80,7 +84,18 @@ class ContentPath:
                 return ParsePathException(f"More points when expected. Expected 3 or less, given {len(path.solit('.'))}")
         return (category, pack, addon, table, note)
 
+    @classmethod
+    def safety(cls, path: str, spare_ctg: str = None, expected: str = None):
+        try:
+            path_object = cls(path, spare_ctg)
+            if expected and not path_object.availables[expected]:
+                raise IntegrityPathException("Expectend end point is not available.")
+            return path_object
+        except (ParsePathException, VerifyPathException, IntegrityPathException):
+            return None
+
+
 
 if __name__ == "__main__":
     print(ContentPath("hello:world.pack.sook"))
-    print(ContentPath("hello").note_exists)
+    print(ContentPath("hello").note_available)
