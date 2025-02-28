0860562b21b765a36415165ffaf619bc524f4a12
petraxeman <pawel.sultanoff@yandex.ru>
2025-02-28 21:35:12 +0300

    Rewrite library.note.delete

--- library/note/delete.py
+++ library/note/delete.py
@@ -1,25 +1,20 @@
 from typing import Union
-from library import contpath
+from library import utils, contpath
 
 
 
 def process(db, data: dict, sender: dict) -> Union[tuple, int]:
-    if not data.get("path"):
-        return {"msg": "Wrong path"}, 401
-    
-    try:
-        path = contpath.ContentPath(data.get("path", ""), "gc:")
-    except contpath.ParsePathException:
+    path = contpath.ContentPath.safety(data.get("path", ""))
+    if not path:
         return {"msg": "Wrong path."}, 401
-    
-    note = db.fetchone("SELECT * FROM notes WHERE path = %s", (path.to_note,))
-    if not note:
-        return {"msg": "Note not found"}, 401
     
     pack = db.fetchone("SELECT * FROM packs WHERE path = %s", (path.to_pack,))
     
-    existed_rights = {"server-admin", "any-create"}.intersection(sender["rights"])
-    if sender["uid"] not in [pack["owner"], *pack["redactors"]] and not existed_rights:
+    if not utils.verify_access(
+        sender["uid"], sender["rights"],
+        {"server-admin", "any-create"},
+        (pack["owner"], *pack["redactors"])):
+
         return {"msg": "You can't do that"}, 401
     
     db.execute("DELETE FROM notes WHERE path = %s", (path.to_note,))
